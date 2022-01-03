7c7b0e3811c1c67cbcf5afbfba5ee413c6a2b277
Pagiestm <92151968+Pagiestm@users.noreply.github.com>
2022-01-03 22:31:03 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,124 @@
+/*!
+ * Start Bootstrap - Shop Homepage v5.0.4 (https://startbootstrap.com/template/shop-homepage)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+ */
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+
+var btnPopup = document.getElementById('btnPopup');
+var overlay = document.getElementById('overlay');
+btnPopup.addEventListener('click', openMoadl);
+function openMoadl() {
+overlay.style.display = 'block';
+}
+var btnClose = document.getElementById('btnClose');
+btnClose.addEventListener('click', closeModal);
+function closeModal() {
+overlay.style.display = 'none';
+}
+
+
+var btnPopup2 = document.getElementById('btnPopup2');
+var overlay2 = document.getElementById('overlay2');
+btnPopup2.addEventListener('click', openMoad2);
+function openMoad2() {
+overlay2.style.display = 'block';
+}
+var btnClose2 = document.getElementById('btnClose2');
+btnClose2.addEventListener('click', closeModal2);
+function closeModal2() {
+overlay2.style.display = 'none';
+}
+
+
+var btnPopup3 = document.getElementById('btnPopup3');
+var overlay3 = document.getElementById('overlay3');
+btnPopup3.addEventListener('click', openMoad3);
+function openMoad3() {
+overlay3.style.display = 'block';
+}
+var btnClose3 = document.getElementById('btnClose3');
+btnClose3.addEventListener('click', closeModal3);
+function closeModal3() {
+overlay3.style.display = 'none';
+}
+
+
+var btnPopup4 = document.getElementById('btnPopup4');
+var overlay4 = document.getElementById('overlay4');
+btnPopup4.addEventListener('click', openMoad4);
+function openMoad4() {
+overlay4.style.display = 'block';
+}
+var btnClose4 = document.getElementById('btnClose4');
+btnClose4.addEventListener('click', closeModal4);
+function closeModal4() {
+overlay4.style.display = 'none';
+}
+
+
+var btnPopup5 = document.getElementById('btnPopup5');
+var overlay5 = document.getElementById('overlay5');
+btnPopup5.addEventListener('click', openMoad5);
+function openMoad5() {
+overlay5.style.display = 'block';
+}
+var btnClose5 = document.getElementById('btnClose5');
+btnClose5.addEventListener('click', closeModal5);
+function closeModal5() {
+overlay5.style.display = 'none';
+}
+
+
+var btnPopup6 = document.getElementById('btnPopup6');
+var overlay6 = document.getElementById('overlay6');
+btnPopup6.addEventListener('click', openMoad6);
+function openMoad6() {
+overlay6.style.display = 'block';
+}
+var btnClose6 = document.getElementById('btnClose6');
+btnClose6.addEventListener('click', closeModal6);
+function closeModal6() {
+overlay6.style.display = 'none';
+}
+
+
+var btnPopup7 = document.getElementById('btnPopup7')
+var overlay7 = document.getElementById('overlay7');
+btnPopup7.addEventListener('click', openMoad7);
+function openMoad7() {
+overlay7.style.display = 'block';
+}
+var btnClose7 = document.getElementById('btnClose7');
+btnClose7.addEventListener('click', closeModal7);
+function closeModal7() {
+overlay7.style.display = 'none';
+}
+
+
+var btnPopup8 = document.getElementById('btnPopup8')
+var overlay8 = document.getElementById('overlay8');
+btnPopup8.addEventListener('click', openMoad8);
+function openMoad8() {
+overlay8.style.display = 'block';
+}
+var btnClose8 = document.getElementById('btnClose8');
+btnClose8.addEventListener('click', closeModal8);
+function closeModal8() {
+overlay8.style.display = 'none';
+}
+
+var btnPopup9 = document.getElementById('btnPopup9')
+var overlay9 = document.getElementById('overlay9');
+btnPopup9.addEventListener('click', openMoad9);
+function openMoad9() {
+overlay9.style.display = 'block';
+}
+var btnClose9 = document.getElementById('btnClose9');
+btnClose9.addEventListener('click', closeModal9);
+function closeModal9() {
+overlay9.style.display = 'none';
+}
